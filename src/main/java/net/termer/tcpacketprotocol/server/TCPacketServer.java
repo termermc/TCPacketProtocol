17a12df8770f6a161eb6a518364836ac65b8180e
= <termer@protonmail.com>
2020-03-31 18:00:28 -0400

    initial commit

---
+++ src/main/java/net/termer/tcpacketprotocol/server/TCPacketServer.java
@@ -0,0 +1,10 @@
+package net.termer.tcpacketprotocol.server;
+
+/**
+ * Main TCPacketProtocol server class. Handles all server functions.
+ * @author termer
+ * @since 1.0
+ */
+public class TCPacketServer {
+	
+}
---
+++ src/test/java/net/termer/tcpacketprotocol/LibraryTest.java
@@ -0,0 +1,16 @@
+package net.termer.tcpacketprotocol;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests basic server capabilities
+ * @author termer
+ * @since 1.0
+ */
+public class LibraryTest {
+    @Test public void testServerStartup() {
+        //
+//        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
